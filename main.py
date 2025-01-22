e571001ad11f401290d9d7a293ffe0d03b695a36
SnakeSnuggles <jacobsylvester6@gmail.com>
2025-01-21 21:42:00 -0700

    changed a bunch of stuff and added changing block states

--- main.py
+++ main.py
@@ -2,35 +2,6 @@ import time
 import pygame 
 import random
 
-
-# env = block_blast_env()
-# 
-# model = PPO("MlpPolicy", env, verbose=1)
-# 
-# model.learn(total_timesteps=10000)
-# 
-# obs, _ = env.reset()
-# 
-# for _ in range(20):
-#     action, _ = model.predict(obs, deterministic=True)  # Predict optimal action
-#     obs, reward, done, _, _ = env.step(action)
-#     env.render()
-#     if done:
-#         break
-# 
-# env.close()
-
-
-'''
-GOALS: 
-    create a window with a grid (8x8)
-'''
-
-pygame.init()
-
-
-
-
 '''
 GOALS: 
     GET MOUSE DETECTION TO WORK 
@@ -38,73 +9,74 @@ GOALS:
     MAYBE GET LINE CLEARS
 
 '''
+ 
+
+
+
 class Block:
-    def __init__(self,image_path:str,pos:tuple):
-        size_image = (50,50)
-        self.image = pygame.image.load(image_path)
+    def __init__(self,state:bool,pos:tuple,size:int=50,):
+        self.size_image = (size,size)
         self.pos = pos
-        self.image = pygame.transform.scale(self.image,size_image)
+        self.state = state
+        self.set(self.state)
+        self.image = pygame.transform.scale(self.image,self.size_image)
+        
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (self.pos[0]*50+20,self.pos[1]*50+40)
 
-        self.image_rect = self.image.get_rect()
-   
-    def change_state(self):
-        ...
-
-    def move(self,x,y):
-        self.image_rect.move(x,y)
-
+    def toggle(self):
+       ... 
+    def set(self,new_state:bool):
+        self.image = pygame.image.load("empty_block.png" if new_state == False else "filled_block.png")
+        self.image = pygame.transform.scale(self.image,self.size_image)
+        self.state = new_state
 
 class Block_Blast:
     def __init__(self):
-        self.size = 8
-        self.board = [[random.choice([True,False]) for x in range(self.size)] for y in range(self.size)] 
-        self.screen = pygame.display.set_mode((450, 510))
+        self.size = 8 
+        self.board:list[Block] = []
+        self.screen = pygame.display.set_mode((500, 500))
         #self.screen = pygame.display.set_mode((1000,1000))
         self.clock = pygame.time.Clock()
         self.running = True
         pygame.font.init()
         self.font = pygame.font.SysFont("Arial",30) 
         self.score = 0
+        for x in range(self.size): 
+            for y in range(self.size):
+               self.board.append(Block(random.choice([True,False]),(x,y))) 
 
-    def run(self):
-        while self.running:
-            # poll for events
-            # pygame.QUIT event means the user clicked X to close your window
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+        pygame.init()
 
-            # fill the screen with a color to wipe away anything from last frame
+    def draw(self):
             self.screen.fill("black")
 
-            # RENDER YOUR GAME HERE
-            # box = pygame.Rect(20,20,50,50)
-            # pygame.draw.rect(self.screen, "black", box)
             
             score_text = self.font.render(f"Score: {self.score}", False, (255,255,255))
 
             self.screen.blit(score_text,(20,0))
             
+            for block in self.board:
+                self.screen.blit(block.image,block.rect)
 
-            blocks_ = []
-            for x in range(self.size):
-                for y in range(self.size):
-                    block = Block("error_block.png",(x,y)) 
-                    if self.board[x][y] == False:
-                        block = Block("empty_block.png",(x,y))
-                    elif self.board[x][y] == True:
-                        block = Block("filled_block.png",(x,y))
-
-                    blocks_.append(block)
-                    self.screen.blit(block.image,block.image_rect.move(x*50+20,y*50+55))
-            
-
-
-
-            # flip() the display to put your work on screen
             pygame.display.flip()
             pygame.display.set_caption(f"FPS: {str(self.clock.get_fps())}")
-            self.clock.tick(60)  # limits FPS to 60
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    good_sprite:list[Block] = [block for block in self.board if block.rect.collidepoint(pos)]
+
+                    print(f"Good sprites: {len(good_sprite)}")
+                    for a in good_sprite:
+                        print("got good_sprites")       
+                        a.set(not a.state)
+
+            self.draw() 
+            self.clock.tick(70)  
 
         pygame.quit()
 
